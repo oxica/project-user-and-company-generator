481de991e0a09825a903ac5dda56c94a6b0ccab9
Oksana <kskaplun@gmail.com>
2022-06-13 22:59:33 +0300

    02

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { User } from "./User";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-console.log("Hello");
-console.log("Hello2");
+// const user = new User();
+// const company = new Company();
+// console.log(user);
+// console.log(company);
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
